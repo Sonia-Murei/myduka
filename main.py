4947ec75dd4ba0366754b429d9f36ef241d7b625
Sonia-Murei <mureisonia@gmail.com>
2026-06-12 16:33:59 +0300

    Posting Data In Flask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import get_products,get_sales,get_stocks
+from flask import Flask, render_template,request,redirect,url_for
+from database import get_products,get_sales,get_stocks,insert_products,insert_sales,insert_stock
 
 # Flask Instance : because "app" is an object and an object is an instance of a class.
 # "Flask" is a class
@@ -15,15 +15,53 @@ def products():
     products_data=get_products()
     return render_template("products.html", products_data = products_data)
 
+@app.route('/add_products',methods=['GET','POST'])
+def add_products():
+    if request.method == 'POST':
+        product_name = request.form['p_name']
+        buying_price = request.form['b_price']
+        selling_price = request.form['s_price']
+
+        new_product=(product_name,buying_price,selling_price)
+        insert_products(new_product)
+        print("Product added successfully")
+
+    return redirect(url_for('products'))
+
 @app.route('/sales')
 def sales():
     sales_data=get_sales()
     return render_template("sales.html",sales_data = sales_data)
 
+@app.route('/add_sales',methods=['GET','POST'])
+def add_sales():
+    if request.method == 'POST':
+        product_id = request.form['pid']
+        quantity = request.form['qt']
+
+        new_sale=(product_id,quantity)
+        insert_sales(new_sale)
+        print("Sale added successfully")
+
+    return redirect(url_for('sales'))
+
+
 @app.route('/stock')
 def stock():
     stocks_data=get_stocks()
     return render_template("stock.html", stocks_data = stocks_data)
+
+@app.route('/add_stocks',methods=['GET','POST'])
+def add_stocks():
+    if request.method == 'POST':
+        product_id = request.form['pid']
+        quantity = request.form['stock_qt']
+
+        new_sale=(product_id,quantity)
+        insert_sales(new_sale)
+        print("Stock added successfully")
+
+    return redirect(url_for('stock'))
 
 @app.route('/dashboard')
 def dashboard():
